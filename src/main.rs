e5e653f4314f983993908348be5bf49a64332f3b
Andezion <245122@edu.p.lodz.pl>
2025-08-12 12:32:48 +0200

    completed sync server

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
-use crate::rune_find1::{rune_find1};
-use crate::rune_find2::{rune_find2};
+use titan_client::TitanApiBlocking;
+use titan_client::TitanBlockingClient;
 
-use titan_client::TitanApi;
-use titan_client::TitanClient;
-use tokio;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-mod rune_find1;
-mod rune_find2;
+    let client = TitanBlockingClient::new("http://localhost:3030");
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let _ = rune_find1();
-    // let _ = rune_find2();
-
-    let client = TitanClient::new("http://localhost:3030");
-
-    let status = client.get_status().await?;
+    let status = client.get_status()?;
     println!("Status: {:?}", status);
 
-
-    let tip = client.get_tip().await?;
+    let tip = client.get_tip()?;
     println!("Block Tip: {:?}", tip);
 
-    let address_data = client.get_address("your-bitcoin-address").await?;
+    let address_data = client.get_address("bcrt1qnx4vqlhu0uk6jehxmvyu93qvqxe3hqw0dz2xzp")?;
     println!("Address Data: {:?}", address_data);
 
     Ok(())
